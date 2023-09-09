a924433019c1762bb07ff5d49832fa17db0a0318
Samarth Parekh <parekh.samarth@gmail.com>
2023-09-09 12:03:39 -0400

    Create test.py

---
+++ Practice Projects/AutoEmail/test.py
@@ -0,0 +1,13 @@
+import smtplib, ssl
+
+port = 465
+smtp_server = "smtp.gmail.com"
+sender_email = "code.dev.test0714"
+reciever_email = "parekh.samarth@gmail.com"
+password = "Soccerrockz@1"
+message = "Hello World!"
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, reciever_email, message)
