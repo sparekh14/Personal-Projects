b8d52f5c562bd3815715a6b440d2f82420644b8d
Samarth Parekh <parekh.samarth@gmail.com>
2024-01-04 16:05:54 +0530

    Delete Sam's Blog/Controllers/Models directory

--- Sam's Blog/Controllers/Models/ViewModels/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace samarth_blog_app.Models.ViewModels;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- Sam's Blog/Controllers/Models/ViewModels/PostViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace samarth_blog_app.Models.ViewModels;
-
-public class PostViewModel
-{
-    public List<PostModel> PostList {get; set;}
-
-    public PostModel Post {get; set;}
-}
