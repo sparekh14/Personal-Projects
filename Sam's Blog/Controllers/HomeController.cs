6d5100d7456fdf9de1f02a6b05e4ef1b3b794721
Samarth Parekh <parekh.samarth@gmail.com>
2024-01-04 16:03:12 +0530

    Create HomeController.cs

---
+++ Sam's Blog/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using samarth_blog_app.Models;
+using samarth_blog_app.Models.ViewModels;
+
+namespace samarth_blog_app.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
